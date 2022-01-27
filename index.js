45a056c5f5c94df4e2717ad3bbb45bdfcbc17d52
CodeWithBayramali <85841871+CodeWithBayramali@users.noreply.github.com>
2022-01-27 06:46:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/category', categoryRouter)
 app.use('/user',userRouter)
 app.use('/comment',commentRouter)
 
-app.listen(process.env.PORT, ()=> {
+app.listen(5000, ()=> {
     
     mongoose.connect(process.env.MONGO_URI,{
         useNewUrlParser: true,
