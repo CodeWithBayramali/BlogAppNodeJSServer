a6ad46d0e7f81a8fa9433ebdd474017b710f2443
CodeWithBayramali <85841871+CodeWithBayramali@users.noreply.github.com>
2022-01-27 06:38:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.listen(process.env.PORT, ()=> {
     mongoose.connect(process.env.MONGO_URI,{
         useNewUrlParser: true,
         useUnifiedTopology: true
-    }).then(()=>{console.log('Connected to Database')}).catch(err=> console.log(err))
+    })
 })
